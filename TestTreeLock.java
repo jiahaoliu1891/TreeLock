0d50fec02f6171c6e25d69bd7c9f9383aa47b828
jiahaoliu1891 <jiahaoliu1891@gmail.com>
2021-10-13 22:11:34 -0400

    first commit

--- TestTreeLock.java
+++ TestTreeLock.java
@@ -9,7 +9,7 @@ class TestTreeLock {
 	
 
 	public static void main(String args[]) throws InterruptedException {
-		Counter ctr = new Counter(numThr);
+		// Counter ctr = new Counter(numThr);
 
        /**  Write code to test the tree lock implementation here.
          *   1. Instantiate an array of Thread objects
@@ -17,19 +17,29 @@ class TestTreeLock {
          *   3. Be sure to wait until all threads join before printing the final count 
          **/
 		Thread threads[] = new Thread[numThr];
-		
-		for (int i = 0; i < numThr; i++) {
-			threads[i] = new Thread(ctr);
-			threads[i].run(); //Run the threads
-		}
+		int numOfTest = 2000;
+		int numOfErr = 0;
+		for (int c = 0; c < numOfTest; c++) {
+			Counter ctr = new Counter(numThr);
+			System.out.println("Start Test @" + c);
+			for (int i = 0; i < numThr; i++) {
+				threads[i] = new Thread(ctr);
+				threads[i].run(); //Run the threads
+			}
+	
+			for (Thread t : threads) {
+				try {
+					t.join();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
 
-		for (Thread t : threads) {
-			try {
-				t.join();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+			if (ctr.getCount() != 1000000) {
+				numOfErr += 1;
+				System.out.println("Error! Counter is " + ctr.getCount());
 			}
 		}
-		System.out.println("Counter is " + ctr.getCount());
+		System.out.printf("%d errors out of %d Tests", numOfErr, numOfTest);
 	}
 }
